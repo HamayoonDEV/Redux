48d02bc3fbd1dabfbd5f59629b7e0199beaccce7
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-14 14:57:33 +0500

    made counter using redux store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state)=>state.counter)
+  const dispatch = useDispatch()
+  const increament=()=>{
+    dispatch({type:"INC"})
+  }
+  const decreament = ()=>{
+    dispatch({type:"DEC"})
+
+  }
+  const add = ()=>{
+    dispatch({type:"ADD",payload:10})
+  }
+
   return (
-    <div className="App">
+    <div >
+      <h1>counter</h1>
+      <h3>{counter}</h3>
+      <button onClick={increament}>+</button>
+      <button onClick={decreament}>-</button>
+      <button onClick={add}>+10</button>
      
     </div>
   );
